be30aa082740be908132d498af95606b0a9785c3
aswinmohanme <git.aswinmohan@gmail.com>
2019-01-21 22:34:26 +0530

    Finish two exercises

---
+++ 1-4.rkt
@@ -0,0 +1,4 @@
+#lang sicp
+(#%require "sicp-library.rkt")
+
+;; Here the function will add the arguments when b is positive and substract when b is negative
---
+++ 1-5.rkt
@@ -0,0 +1,3 @@
+#lang sicp
+
+;;; When the given function is evaluated in normal order, it runs in an infinite loop whereas in applicative order it terminates at 0
